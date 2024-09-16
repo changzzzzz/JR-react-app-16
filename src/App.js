5a979d63c9838220e8427157b1c38368c2d78867
Yang Liu <youngliu0614@gmail.com>
2024-09-16 21:10:14 +1000

    course difficulty with different button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,82 @@
 import logo from './logo.svg';
 import './App.scss';
+import { useState } from "react";
 import CourseCard from './component/course-card';
 
 function App() {
+
+  const [cardList,setCardList] = useState([
+
+    {
+        id:1,
+        title:".Net", 
+        price:1000, 
+        language:"en", 
+        duration:"3 month", 
+        location:"Mel",
+        recentAdded:false,
+        difficulty:"Beginner", 
+    },
+    {
+        id:2,
+        title:"React", 
+        price:1000, 
+        language:"en", 
+        duration:"3 month", 
+        location:"Mel",
+        recentAdded:true,
+        difficulty:"Intermediate"
+    },
+    {
+        id:3,
+        title:"HTML", 
+        price:1000, 
+        language:"en", 
+        duration:"3 month", 
+        location:"Mel",
+        recentAdded:false,
+        difficulty: "Advanced"
+    },        
+    {
+        id:4,
+        title:"Linkedin", 
+        price:1000, 
+        language:"en", 
+        duration:"3 month", 
+        location:"Mel",
+        recentAdded:true,
+        difficulty:"Advanced"
+    },
+    
+])
+
+
   return (
     <div className="App">
-      <CourseCard></CourseCard>
+      <div className="card-container">
+        {
+          cardList.map(
+            item => {
+              return(
+                <CourseCard title={item.title} 
+                price={item.price} 
+                language={item.language}
+                duration={item.duration}
+                location={item.location}
+                recentAdded={item.recentAdded}
+                difficulty={item.difficulty} ></CourseCard>
+              )
+            }
+          )
+          
+        }
+        
+      </div>
+      
     </div>
   );
 }
+
+
 
 export default App;
--- src/component/course-card/index.jsx
+++ src/component/course-card/index.jsx
@@ -3,74 +3,96 @@ import "./index.scss";
 
 export default props => {
 
-    const [cardList,setCardList] = useState([
+    // const [cardList,setCardList] = useState([
 
-        {
-            id:1,
-            title:".Net", 
-            price:1000, 
-            language:"en", 
-            duration:"3 month", 
-            location:"Mel",
-            recentAdded:false
-        },
-        {
-            id:2,
-            title:"React", 
-            price:1000, 
-            language:"en", 
-            duration:"3 month", 
-            location:"Mel",
-            recentAdded:true
-        },
-        {
-            id:3,
-            title:"HTML", 
-            price:1000, 
-            language:"en", 
-            duration:"3 month", 
-            location:"Mel",
-            recentAdded:false
-        },        
-        {
-            id:4,
-            title:"Linkedin", 
-            price:1000, 
-            language:"en", 
-            duration:"3 month", 
-            location:"Mel",
-            recentAdded:true
-        }
-    ])
+    //     {
+    //         id:1,
+    //         title:".Net", 
+    //         price:1000, 
+    //         language:"en", 
+    //         duration:"3 month", 
+    //         location:"Mel",
+    //         recentAdded:false,
+    //         difficulty:1
+    //     },
+    //     {
+    //         id:2,
+    //         title:"React", 
+    //         price:1000, 
+    //         language:"en", 
+    //         duration:"3 month", 
+    //         location:"Mel",
+    //         recentAdded:true,
+    //         difficulty:2
+    //     },
+    //     {
+    //         id:3,
+    //         title:"HTML", 
+    //         price:1000, 
+    //         language:"en", 
+    //         duration:"3 month", 
+    //         location:"Mel",
+    //         recentAdded:false,
+    //         difficulty:3
+    //     },        
+    //     {
+    //         id:4,
+    //         title:"Linkedin", 
+    //         price:1000, 
+    //         language:"en", 
+    //         duration:"3 month", 
+    //         location:"Mel",
+    //         recentAdded:true,
+    //         difficulty:4
+    //     },
+        
+    // ])
+
+
 
     return (
-        <div className="card-container" >
-            {
-                cardList.map(   
-                    item => {
-                        return (
-                            <div class="card card-item" style={{width: "18rem"}}>
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
-                                <div class="card-body">
-                                    <h5 class="card-title">{item.title}</h5>
-                                    <p class="card-text">price：{item.price}</p>
-                                    <p class="card-text">language：{item.language}</p>
-                                    <p class="card-text">duration：{item.duration}</p>
-                                    <p class="card-text">location：${item.location}</p>
-                                    {item.recentAdded?<a href="#" class="btn btn-primary">New</a>:<></>}
-                                    
-                                </div>
-                            </div>
-                        )
-                    }
-                )
-            }
-        
-        
-
-        
-       
-
+        <div class="card card-item" style={{width: "18rem"}}>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+            <div class="card-body">
+                <h5 class="card-title">{props.title}</h5>
+                <p class="card-text">Price:{props.price}</p>
+                <p class="card-text">Language:{props.language}</p>
+                <p class="card-text">Duration:{props.duration}</p>
+                <p class="card-text">Location:{props.location}</p>
+                <p class="card-text">Difficulty:{props.difficulty}</p>
+                {props.difficulty === "Beginner" ?  <a href="#" class="btn btn-primary">Start Learning Now!</a>:
+                <a href="#" class="btn btn-primary">Enroll Now</a>}
+               
+            </div>
         </div>
     )
+                    
+                
+
+
+    // return (
+    //     <div className="card-container">
+    //         {
+    //             cardList.map(   
+    //                 item => {
+    //                     return (
+    //                         <div class="card card-item" style={{width: "18rem"}}>
+    //                             <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+    //                             <div class="card-body">
+    //                                 <h5 class="card-title">{item.title}</h5>
+    //                                 <p class="card-text">price：{item.price}</p>
+    //                                 <p class="card-text">language：{item.language}</p>
+    //                                 <p class="card-text">duration：{item.duration}</p>
+    //                                 <p class="card-text">location：{item.location}</p>
+    //                                 {item.recentAdded?<a href="#" class="btn btn-primary">New</a>:<></>}
+                                    
+    //                             </div>
+    //                         </div>
+    //                     )
+    //                 }
+    //             )
+    //         }
+
+    //     </div>
+    // )
 }
